2c6a877e58acb81db40991bf0e841acdbd13deec
Bla He Hir <Xer6221@gmail.com>
2017-12-08 13:44:16 -0500

    Add files via upload

---
+++ Battery_checker1.ino
@@ -0,0 +1,28 @@
+float e = 0;//analogRead(1)
+float r = 0;//analogRead(2)
+float t = 0;//analogRead(3)
+float s = 0;//Battery cell 1
+float d = 0;//Battery cell 2
+float f = 0;//Battery cell 3
+void setup() {
+  Serial.begin(9600);
+  analogReference(INTERNAL);//
+
+}
+
+void loop() {
+e = analogRead(1);
+r = analogRead(2);
+t = analogRead(3);
+
+s = e * 4.2;
+s = s / 969;//equation for Battery cell 1
+
+
+
+Serial.print(s);//Battery cell 1
+Serial.print(',');
+Serial.print(d);//Battery cell 2
+Serial.print(',');
+Serial.println(f);//Battery cell 3
+}
---
+++ Test1.ino
@@ -0,0 +1,29 @@
+int x=0;
+void setup() {
+
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, INPUT);
+}
+
+void loop() {
+
+  if (digitalRead(5) == LOW) {
+    delay(100); 
+    x = !x;
+    digitalWrite(3, x);
+    digitalWrite(4, x);
+  }
+}
+
+
+
+/*
+if (digitalRead(switchPin) == LOW)
+  {  // switch is pressed - pullup keeps pin high normally
+    delay(100);                        // delay to debounce switch
+    running = !running;                // toggle running variable
+    digitalWrite(LEDpin, running);      // indicate via LED
+  }
+
+  */
